f77f38b7b4e162cf8b025437994bf7582b096659
DavidVentura <davidventura27@gmail.com>
2022-06-10 12:52:02 +0200

    compile and run generated code; compare output to expected

--- tests.py
+++ tests.py
@@ -5,12 +5,18 @@ import subprocess
 
 import pytest
 
-from a import transform
+from a import transform, Function
+from luaparser.astnodes import Type
 
 here = Path(__file__).parent
 
-def _compile_and_run(source: Path, dest_dir: Path):
-    s = subprocess.check_output(['g++', '-std=c++17', f'-I{here.absolute()}', str(source.absolute())], cwd=dest_dir)
+def _compile_and_run(transformed_src: str, dest_dir: Path):
+    _target_temp = Path(dest_dir / 'out.cpp')
+
+    with _target_temp.open('w') as fd:
+        fd.write(transformed_src)
+
+    s = subprocess.check_output(['g++', '-std=c++17', f'-I{here.absolute()}', str(_target_temp)], cwd=dest_dir)
     s = subprocess.check_output(['./a.out'], cwd=dest_dir)
     return s.decode().strip().splitlines()
 
@@ -19,14 +25,17 @@ def find_case_pairs():
             for d in os.listdir('test_cases')]
 
 @pytest.mark.parametrize("in_f,expected_f,stdout_f", find_case_pairs())
-def test_cases(in_f: Path, expected_f: Path, stdout_f: Path):
+def test_cases(in_f: Path, expected_f: Path, stdout_f: Path, monkeypatch):
     print(in_f, expected_f)
     with in_f.open() as fd:
         i = fd.read()
     with expected_f.open() as fd:
         expected = fd.read()
 
-    actual = transform(i).strip().splitlines()
+    code = transform(i).strip()
+    actual = code.splitlines()
+    with open('temp_out.cpp', 'w') as fd:
+        print(code, file=fd)
     assert actual == expected.strip().splitlines()
 
     if stdout_f is None:
@@ -35,8 +44,11 @@ def test_cases(in_f: Path, expected_f: Path, stdout_f: Path):
     with stdout_f.open() as fd:
         expected_stdout = fd.read()
 
+    _patched_src = transform(i, testing=True).strip()
+    with open('patched_out.cpp', 'w') as fd:
+        print(_patched_src, file=fd)
 
     with tempfile.TemporaryDirectory() as td:
-        exec_output = _compile_and_run(expected_f, Path(td))
+        exec_output = _compile_and_run(_patched_src, Path(td))
 
     assert expected_stdout.strip().splitlines() == exec_output
