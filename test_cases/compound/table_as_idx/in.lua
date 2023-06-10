f539a1d953a182d31c716af7b1fa8522ba2ecc42
DavidVentura <davidventura27@gmail.com>
2023-06-10 23:37:07 +0200

    add new simple compound case

---
+++ test_cases/compound/table_as_idx/in.lua
@@ -0,0 +1,6 @@
+function main()
+	member  = {x=1}
+	empty   = {}
+	member[empty] = 5
+	print(member[empty])
+end
---
+++ test_cases/compound/table_as_idx/out.c
@@ -0,0 +1,16 @@
+#include "lua.c"
+TValue_t empty;
+TValue_t member;
+TValue_t __preinit();
+TValue_t __main();
+
+TValue_t __main() {
+  member = TTAB(make_table(4));
+  set_tabvalue(member.table, TSTR("x"), TNUM16(1));
+  ;
+  empty = TTAB(make_table(4));
+  set_tabvalue(member.table, empty, TNUM16(5));
+  print(get_tabvalue(member.table, empty));
+}
+
+TValue_t __preinit() {}
