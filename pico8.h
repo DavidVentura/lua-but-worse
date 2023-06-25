984fcc7754cbfb8f19d0b2e9dc0a3b0e38cde1a1
DavidVentura <davidventura27@gmail.com>
2023-06-25 12:57:55 +0200

    add placeholder values

--- pico8.h
+++ pico8.h
@@ -1,8 +1,21 @@
+#include "assert.h"
 #include "lua.c"
 
-#define stdlib(x) TValue_t x(uint8_t c, TValue_t*);
+#define stdlib(x) TValue_t x(uint8_t argc, TValue_t* argv)
 
 stdlib(btn);
 stdlib(map);
 stdlib(spr);
 stdlib(cls);
+
+stdlib(btn) {
+	assert(argc==1);
+	TValue_t b = argv[0];
+	return TNUM(1);
+}
+stdlib(spr) {
+}
+stdlib(map) {
+}
+stdlib(cls) {
+}
