d28b96ec0de39315314aac596bde8906baec543b
DavidVentura <davidventura27@gmail.com>
2023-06-30 23:53:39 +0200

    fix forloop limit

--- test_cases/basic/forloop/out.c
+++ test_cases/basic/forloop/out.c
@@ -4,7 +4,7 @@ TValue_t __preinit();
 TValue_t __main();
 
 TValue_t __main() {
-  for (TValue_t i = TNUM16(1); __bool(_lt(i, TNUM16(7))); i = _add(i, TNUM16(1))) {
+  for (TValue_t i = TNUM16(1); __bool(_leq(i, TNUM16(7))); i = _add(i, TNUM16(1))) {
     printh(i);
   }
   return TNUM16(0);
