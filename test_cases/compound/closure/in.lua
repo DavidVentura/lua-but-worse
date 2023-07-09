a4d1008340efde6f3fa614626024095226550fd4
DavidVentura <davidventura27@gmail.com>
2023-07-09 11:25:04 +0200

    add test-case from docs

--- test_cases/compound/closure/in.lua
+++ test_cases/compound/closure/in.lua
@@ -1,18 +1,34 @@
+function newCounter ()
+  local i = 0
+  return function ()   -- anonymous function
+    i = i + 1
+    return i
+  end
+end
+
 function main()
-	local captured = 7
-	a = function(x)
-		return x*captured
-	end
-	printh(a(5))
+  local captured = 7
+  a = function(x)
+    return x*captured
+  end
+  printh(a(5))
 
-	captured = 1
-	printh(a(5))
-	function b()
-		captured = 2
-	end
-	b()
-	printh(a(5))
+  captured = 1
+  printh(a(5))
+  function b()
+    captured = 2
+  end
+  b()
+  printh(a(5))
 
-	return 0
+  printh("newCounter")
+  c1 = newCounter()
+  printh(c1())  --> 1
+  printh(c1())  --> 2
+  c2 = newCounter()
+  printh(c2())  --> 1
+  printh(c1())  --> 3
+  printh(c2())  --> 2
+  return 0
 end
 
--- test_cases/compound/closure/out.c
+++ test_cases/compound/closure/out.c
@@ -3,13 +3,28 @@
 #include "lua_table.h"
 #include "pico8.h"
 #include "stdlib.h"
+TValue_t __str_newCounter = T_NULL;
 TValue_t __str_captured = T_NULL;
+TValue_t __str_i = T_NULL;
+TValue_t c2 = T_NULL;
+TValue_t c1 = T_NULL;
 TValue_t b = T_NULL;
 TValue_t a = T_NULL;
 TValue_t __preinit();
 TValue_t __nested_func_b(TVSlice_t function_arguments);
 TValue_t __anonymous_function_a(TVSlice_t function_arguments);
+TValue_t __anonymous_function_0(TVSlice_t function_arguments);
 TValue_t __main();
+TValue_t newCounter(TVSlice_t function_arguments);
+
+TValue_t newCounter(TVSlice_t function_arguments) {
+  TValue_t gc lambda_args = T_NULL;
+  _set(&lambda_args, TTAB(make_table(1)));
+  _set(&lambda_args, TTAB(make_table(1)));
+  _set(&lambda_args, TTAB(make_table(1)));
+  set_tabvalue(lambda_args, __str_i, TNUM16(0));
+  return TCLOSURE(__anonymous_function_0, lambda_args);
+}
 
 TValue_t __main() {
   TValue_t gc lambda_args = T_NULL;
@@ -23,7 +38,22 @@ TValue_t __main() {
   _set(&b, TCLOSURE(__nested_func_b, lambda_args));
   CALL((b), ((TVSlice_t){.elems = NULL, .num = 0}));
   printh(CALL((a), ((TVSlice_t){.elems = (TValue_t[1]){TNUM16(5)}, .num = 1})));
+  printh(__str_newCounter);
+  _set(&c1, CALL((newCounter), ((TVSlice_t){.elems = NULL, .num = 0})));
+  printh(CALL((c1), ((TVSlice_t){.elems = NULL, .num = 0})));
+  printh(CALL((c1), ((TVSlice_t){.elems = NULL, .num = 0})));
+  _set(&c2, CALL((newCounter), ((TVSlice_t){.elems = NULL, .num = 0})));
+  printh(CALL((c2), ((TVSlice_t){.elems = NULL, .num = 0})));
+  printh(CALL((c1), ((TVSlice_t){.elems = NULL, .num = 0})));
+  printh(CALL((c2), ((TVSlice_t){.elems = NULL, .num = 0})));
   return TNUM16(0);
+}
+
+TValue_t __anonymous_function_0(TVSlice_t function_arguments) {
+  TValue_t gc lambda_args = T_NULL;
+  _set(&lambda_args, __get_array_index_capped(function_arguments, 0));
+  set_tabvalue(lambda_args, __str_i, _add(get_tabvalue(lambda_args, __str_i), TNUM16(1)));
+  return get_tabvalue(lambda_args, __str_i);
 }
 
 TValue_t __anonymous_function_a(TVSlice_t function_arguments) {
@@ -41,6 +71,8 @@ TValue_t __nested_func_b(TVSlice_t function_arguments) {
 }
 
 TValue_t __preinit() {
-  _grow_strings_to(1);
-  _set(&__str_captured, TSTRi(_store_str_at_or_die(CONSTSTR("captured"), 0)));
+  _grow_strings_to(3);
+  _set(&__str_i, TSTRi(_store_str_at_or_die(CONSTSTR("i"), 2)));
+  _set(&__str_captured, TSTRi(_store_str_at_or_die(CONSTSTR("captured"), 1)));
+  _set(&__str_newCounter, TSTRi(_store_str_at_or_die(CONSTSTR("newCounter"), 0)));
 }
