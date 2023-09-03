220df5c0f8061a00cdf381ca0453c0344d8bfc74
DavidVentura <davidventura27@gmail.com>
2023-09-03 17:43:21 +0200

    also add ONE (1) constant

--- fix32.h
+++ fix32.h
@@ -12,6 +12,7 @@ typedef struct __attribute__((__packed__))fix32_s {
 
 
 static const fix32_t __ZERO = (fix32_t){.i=0, .f=0};
+static const fix32_t __ONE = (fix32_t){.i=1, .f=0};
 // grep -P '^\w+.*{$' fix32.c | sed 's/\s\+{/;/'
 
 int32_t fix32_to_int32(fix32_t f);
